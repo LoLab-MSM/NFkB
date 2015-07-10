97c7aceed29a17a1db025e12c4e7f14f1fd4341b
Geena Ildefonso <geena.ildefonso@ucf.edu>
2015-07-10 18:40:20 -0500

    removed code for simulation

--- NFkB_Messy_Model.py
+++ NFkB_Messy_Model.py
@@ -213,7 +213,7 @@ Observable('IkBa_NFkB_obs', IkBa_NFkB())
 Observable('IkBan_NFkBn_obs', IkBan_NFkBn())
 Observable('IKKn_obs', IKKn()) #
 
-generate_equations(model, verbose=True)
+#generate_equations(model, verbose=True)
 
 # for i,sp in enumerate(model.species):
 #     print i,":",sp
@@ -223,80 +223,3 @@ generate_equations(model, verbose=True)
 # print
 # for i,ode in enumerate(model.odes):
 #     print i,":",ode
-
-
-# Simulate the model
-time = np.linspace(0, 18000, 1801)
-
-# ODE simulation
-#@TODO fix array problem
-# param_values = np.array([p.value for p in model.parameters])
-# param_values[18]=0.1
-plt.figure(1)
-x = odesolve(model, time, verbose=True) #integrator='lsoda',
-for obs in ["IKKKa_obs", "IKKii_obs", "IKKa_obs", "IKKi_obs", "IkBa_p_obs", "IkBan_NFkBn_obs"]:
-    plt.plot(time, x[obs], label=re.match(r"(\w+)_obs", obs).group(), linewidth=3)
-    # re.match(r"(\w+)_obs", obs).group()
-    #plt.plot(time/60, x[obs.name], label=obs.name)
-    plt.legend(loc=0, prop={'size': 16})
-plt.xlabel("Time (in minutes)", fontsize=16)
-plt.ylabel("Concentrations", fontsize=16)
-plt.xticks(fontsize=10)
-plt.yticks(fontsize=10)
-
-# plt.figure()
-# for i in range(len(model.species)):
-#     plt.plot(time/60, x["__s%d" %i])
-plt.figure(2)
-for obs in ["TNFR1a_obs", "A20_on_obs"]:
-    plt.plot(time, x[obs], label=re.match(r"(\w+)_obs", obs).group(), linewidth=3)
-    plt.legend(loc=0, prop={'size': 16})
-plt.xlabel("Time (in minutes)", fontsize=16)
-plt.ylabel("Concentrations", fontsize=16)
-plt.xticks(fontsize=10)
-plt.yticks(fontsize=10)
-
-plt.figure(3)
-for obs in ["A20_obs", "A20_off_obs", "IkBa_obs", "IkBan_obs"]:
-    plt.plot(time, x[obs], label=re.match(r"(\w+)_obs", obs).group(), linewidth=3)
-    plt.legend(loc=0, prop={'size': 16})
-plt.xlabel("Time (in minutes)", fontsize=16)
-plt.ylabel("Concentrations", fontsize=16)
-plt.xticks(fontsize=10)
-plt.yticks(fontsize=10)
-
-plt.figure(4)
-plt.plot(time/60, x["NFkBn_obs"], label=NFkBn_obs)
-plt.xlabel("Time (in minutes)", fontsize=16)
-plt.ylabel("Concentration", fontsize=16)
-plt.legend()
-
-plt.figure(5)
-plt.plot(time/60, x["IkBa_off_obs"], label=IkBa_off_obs)
-plt.xlabel("Time (in minutes)", fontsize=16)
-plt.ylabel("Concentration", fontsize=16)
-plt.legend()
-
-plt.figure(6)
-plt.plot(time/60, x["IkBa_on_obs"], label=IkBa_on_obs)
-plt.xlabel("Time (in minutes)", fontsize=16)
-plt.ylabel("Concentration", fontsize=16)
-plt.legend()
-
-
-# # SSA simulation
-# x = run_ssa(model, time, verbose=True)
-# plt.figure('SSA')
-# plt.plot(time, x['B_obs'], label=B_obs, lw=2)
-# plt.xlabel('Time (seconds)')
-# plt.ylabel('Amount of B_obs')
-# plt.legend(loc=0)
-
-# plt.figure('ODE')
-# plt.plot(time, x['IKKKa_obs'], label=IKKKa_obs, lw=2)
-# plt.xlabel('Time (seconds)')
-# plt.ylabel('Amount of IKKKa_obs')
-# plt.legend(loc=0)
-#plt.show()
-
-
