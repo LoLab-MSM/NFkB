6d3edd2115425afb30cdd11299b7d7b7448336da
ildefog <ildefog@puma.mc.vanderbilt.edu>
2017-02-15 17:36:13 -0600

    PSO results

--- Hoffman/pso_nnrm.py
+++ Hoffman/pso_nnrm.py
@@ -190,14 +190,14 @@ if __name__ == '__main__':
         os.mkdir(out_dir)
 
     # create 100 best fits parameter sets
-    for i in range(1000):
+    for i in range(100):
         pso_fn = PSO(save_sampled=False, verbose=False, num_proc=16)
         print("running {}".format(i))
         pso_fn.set_cost_function(likelihood)
         pso_fn.set_start_position(xnominal)
         pso_fn.set_bounds(3)
         pso_fn.set_speed(-.25, .25)
-        pso_fn.run(100, 500)  # particles in swarm and iterations
+        pso_fn.run(50, 100)  # particles in swarm and iterations
         print("Iteration = {} : best value = {}".format(i, pso_fn.best.fitness.values[0]))
         csv_out = os.path.join(out_dir, 'best_fit_{}.csv'.format(i))
         fig_out = os.path.join(out_dir, 'best_fit_{}'.format(i))
