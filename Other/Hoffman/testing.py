981f133ba1a281bf8c21259897a971a8a797f222
geenaildefonso <geena.ildefonso@ucf.edu>
2019-02-10 00:52:40 -0600

    deleting not needed folders, updating run code for nfkb

--- Other/Hoffman/testing.py
+++
@@ -1,4 +0,0 @@
-
-
-
-Rule('EGF_bind_EGFR', EGF(begfr = None) + EGFR(begf = None) <> EGF(begfr = 1) % EGFR(begf = 1))
--- Other/Misc/Old NFkB/example.py
+++
@@ -1,4 +0,0 @@
-result = []
-with open('_diswitch.txt','r') as inputFile:
-    for line in inputFile:
-        result.append(float(line))
