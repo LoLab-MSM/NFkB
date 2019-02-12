952ac3b2ca285c2215d2c94ed44141291e788bb7
geenaildefonso <geena.ildefonso@ucf.edu>
2019-02-12 10:48:40 -0600

    new file for marco nfkb code

--- Marco_NFkB/nfkb_marco.py
+++ Marco_NFkB/nfkb_marco.py
@@ -10,6 +10,8 @@ from scipy import interpolate
 from scipy.interpolate import *
 import scipy.interpolate
 import numpy as np
+from pysb.simulator.bng import BngSimulator
+
 Model ()
 
 #Declaration of monomers
@@ -390,3 +392,58 @@ Observable('IkBd_obs', IkBd(nfkb = None, S='C'))
 
 generate_equations(model)
 
+tspan = np.linspace(0, 720, 721)
+sim = BngSimulator(model, tspan = tspan)
+simulation_result = sim.run(method='ode')
+
+plt.figure(figsize = (15,10))
+# plt.figure()
+plt.subplot(231)
+plt.plot(tspan/60, simulation_result.observables['TNF_obs'], label = 'TNF')
+plt.xlabel("Time (in hr)", fontsize=10)
+plt.ylabel("Concentration uM", fontsize=10)
+# plt.ylim(ymin = -10, ymax =100)
+plt.legend(loc=0)
+
+# plt.figure()
+plt.subplot(232)
+plt.plot(tspan/60, simulation_result.observables['TNFR_obs'], label = 'TNFR')
+plt.xlabel("Time (in hr)", fontsize=10)
+plt.ylabel("Concentration uM", fontsize=10)
+# plt.ylim(ymin = -10, ymax =100)
+plt.legend(loc=0)
+
+# plt.figure()
+plt.subplot(233)
+plt.plot(tspan/60, simulation_result.observables['IKKa_obs'],label = 'IKKa')
+plt.xlabel("Time (in hr)", fontsize=10)
+plt.ylabel("Concentration uM", fontsize=10)
+# plt.ylim(ymin = -10, ymax =100)
+plt.legend(loc=0)
+
+plt.subplot(234)
+plt.plot(tspan/60, simulation_result.observables['NFkBn_obs'], label = 'NFkBn')
+plt.xlabel("Time (in hr)", fontsize=10)
+plt.ylabel("Concentration uM", fontsize=10)
+# plt.ylim(ymin = -10, ymax =100)
+plt.legend(loc=0)
+
+# plt.figure()
+plt.subplot(235)
+plt.plot(tspan/60, simulation_result.observables['obs_A20t'], label = 'A20')
+
+plt.xlabel("Time (in hr)", fontsize=10)
+plt.ylabel("Concentration uM", fontsize=10)
+# plt.ylim(ymin = -10, ymax =100)
+plt.legend(loc=0)
+
+# plt.figure()
+plt.subplot(236)
+plt.plot(tspan/60, simulation_result.observables['IkBa_obs'],label = 'IkBa')
+plt.xlabel("Time (in hr)", fontsize=10)
+plt.ylabel("Concentration uM", fontsize=10)
+# plt.ylim(ymin = -10, ymax =100)
+plt.legend(loc=0)
+plt.savefig('bng_nfkb')
+plt.tight_layout()
+plt.show()
